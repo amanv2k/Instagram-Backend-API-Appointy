e8c95ff2f9936389fb7c0b80c9cfa9a55cf3759c
aman-verma2019 <aman.verma2019@vitstudent.ac.in>
2021-10-09 19:01:32 +0530

    locally hosted api

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # Instagram-Backend-API-Appointy
 Git repository for appointy TECH task
+
+
+## Run
+Run server with login credentials as a env variables
+`USER_EMAIL=test USER_PW=test go run main.go
+curl localhost:8080/user -u test:test`
+
+![local-login](img/local-login.png)
+
+---
+
+POST a post
+`curl localhost:8080/post -X POST -d '{"Caption":"This is a caption for some other image","ImageURL":"SomeOtherImageLink","ID":"id_3","PostTS":"2009-11-10 02:00:00 +0000 UTC m=+0.000000001"}' -H "Content-Type: application/json"`
+
+![POST-post](img/post-posts.png)
+
+---
+
+Get all the existing posts
+`curl localhost:8080/post`
+
+![get-posts](img/get-posts.png)
+
+---
+
+GET a post by ID
+`curl localhost:8080/posts/1633784307862447000`
+![get-post-by-id](img/get-post-by-id.png)
+
+---
+
+### Completed tasks
+
+- [x] Create an User
+- [ ] Get a user using id
+- [x] Create a Post
+- [x] Get a post using id
+- [x] List all posts
+
+## Note - API has been developed for local purposes only as of now.
+
--- main.go
+++ main.go
@@ -1,65 +1,185 @@
 package main
 
 import (
-	"context"
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
+	"os"
+	"strings"
+	"sync"
 	"time"
-
-	"go.mongodb.org/mongo-driver/bson/primitive"
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-type Users struct {
-	ID       primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
-	Name     string             `json:"Name,omitempty" bson:"Name,omitempty"`
-	Email    string             `json:"Email,omitempty" bson:"Email,omitempty"`
-	Password string             `json:"Password,omitempty" bson:"Password,omitempty"`
+type Post struct {
+	Caption  string `json:"caption"`
+	ImageURL string `json:"imageURL"`
+	ID       string `json:"id"`
+	PostTS   string `json:"postTs"`
 }
 
-var client *mongo.Client
-
-type MyMux struct {
+type postsHandlers struct {
+	sync.Mutex
+	store map[string]Post
 }
 
-func (p *MyMux) ServeHTTP(response http.ResponseWriter, request *http.Request) {
-	if request.URL.Path == "/" {
-		// sayhelloName(w, r)
-		fmt.Println("mux active")
+func (h *postsHandlers) posts(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		h.get(w, r)
 		return
-	} else if request.URL.Path == "/users" {
-		fmt.Printf("users mux")
-		CreateUserEndpoint(response, request)
+	case "POST":
+		h.post(w, r)
+		return
+	default:
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		w.Write([]byte("method not allowed"))
 		return
 	}
-	http.NotFound(response, request)
-	return
 }
 
-func CreateUserEndpoint(response http.ResponseWriter, request *http.Request) {
-	if request.Method == "POST" {
-		response.Header().Add("content-type", "application/json")
-		var users Users
-		json.NewDecoder(request.Body).Decode((&users))
-		collection := client.Database("testdb").Collection("testcollection")
-		ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
-		result, _ := collection.InsertOne(ctx, users)
-		json.NewEncoder(response).Encode(result)
-	} else {
-		fmt.Printf("Method not allowed")
-		http.Redirect(response, request, "/", http.StatusFound)
+func (h *postsHandlers) get(w http.ResponseWriter, r *http.Request) {
+	posts := make([]Post, len(h.store))
+
+	h.Lock()
+	i := 0
+	for _, post := range h.store {
+		posts[i] = post
+		i++
+	}
+	h.Unlock()
+
+	jsonBytes, err := json.Marshal(posts)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
 	}
 
+	w.Header().Add("content-type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(jsonBytes)
+}
+
+func (h *postsHandlers) getPosts(w http.ResponseWriter, r *http.Request) {
+	parts := strings.Split(r.URL.String(), "/")
+	if len(parts) != 3 {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	h.Lock()
+	post, ok := h.store[parts[2]]
+	h.Unlock()
+	if !ok {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	jsonBytes, err := json.Marshal(post)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+	}
+
+	w.Header().Add("content-type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(jsonBytes)
+}
+
+func (h *postsHandlers) post(w http.ResponseWriter, r *http.Request) {
+	bodyBytes, err := ioutil.ReadAll(r.Body)
+	defer r.Body.Close()
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		return
+	}
+
+	ct := r.Header.Get("content-type")
+	if ct != "application/json" {
+		w.WriteHeader(http.StatusUnsupportedMediaType)
+		w.Write([]byte(fmt.Sprintf("need content-type 'application/json', but got '%s'", ct)))
+		return
+	}
+
+	var post Post
+	err = json.Unmarshal(bodyBytes, &post)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(err.Error()))
+		return
+	}
+
+	post.ID = fmt.Sprintf("%d", time.Now().UnixNano())
+	h.Lock()
+	h.store[post.ID] = post
+	defer h.Unlock()
+}
+
+func newPostHandlers() *postsHandlers {
+	return &postsHandlers{
+		store: map[string]Post{
+			"id1": Post{
+				Caption:  "This is a caption",
+				ImageURL: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1200px-Image_created_with_a_mobile_phone.png",
+				ID:       "id_1",
+				PostTS:   "2009-11-10 23:00:00 +0000 UTC m=+0.000000001",
+			},
+			"id2": Post{
+				Caption:  "This is a caption for second pic",
+				ImageURL: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b3/Wikipedia-logo-v2-en.svg/1200px-Wikipedia-logo-v2-en.svg.png",
+				ID:       "id_2",
+				PostTS:   "2010-12-10 23:50:00 +0000 UTC m=+0.000000001",
+			},
+		},
+	}
+
+}
+
+type User struct {
+	ID       string
+	Name     string
+	Email    string
+	password string
+}
+
+func newUserPortal() *User {
+	Email := os.Getenv("USER_EMAIL")
+	password := os.Getenv("USER_PW")
+	if Email == "" || password == "" {
+		panic("required env not provided")
+	}
+	return &User{Email: Email, password: password}
+}
+
+func (a User) handler(w http.ResponseWriter, r *http.Request) {
+	user, pass, ok := r.BasicAuth()
+	if !ok || user != a.Email || pass != a.password {
+		w.WriteHeader(http.StatusUnauthorized)
+		w.Write([]byte("401 - unauthorized"))
+		return
+	}
+
+	w.Write([]byte("<html><h1>User portal</h1></html>"))
 }
 
 func main() {
-	fmt.Println("Starting the app...")
-	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
-	client, _ = mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
-	router := &MyMux{}
-	http.HandleFunc("/users", CreateUserEndpoint)
-	http.ListenAndServe(":12345", router)
+	user := newUserPortal()
+	postsHandlers := newPostHandlers()
+	http.HandleFunc("/post", postsHandlers.posts)
+	http.HandleFunc("/posts/", postsHandlers.getPosts)
+	http.HandleFunc("/user", user.handler)
 
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		panic(err)
+	}
+
+	// ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	// defer cancel()
+	// client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb+srv://brianv2k:<password>@ig-api.xw8fz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"))
+	// err := http.ListenAndServe(":8080", nil)
+	// 	if err != nil {
+	// 		panic(err)
+	// 	}
 }
